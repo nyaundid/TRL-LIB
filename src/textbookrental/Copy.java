1af0d4ab0ba3950d2c909f6896003358a09615d4
yemmene <yemmene@gmail.com>
2018-04-04 19:46:42 -0500

    Updated classes

--- src/textbookrental/Copy.java
+++ src/textbookrental/Copy.java
@@ -1,5 +1,31 @@
-package textbookrental;
-
+//Best assignment
 public class Copy {
+	private String copyID;
+	private Patron PatronCheckOut;
 
+	public Copy (String c)
+	{
+		copyID=c;
+	}
+
+	public String getCopyID() {
+		return copyID;
+	}
+	
+	public void setCopyTo(Patron p)
+	{
+		PatronCheckOut=p;
+	}
+	
+	public Patron getCopyTo()
+	{
+		return PatronCheckOut;
+	}
+	
+	public String toString()
+	{
+		return "\t Copy id:" + copyID + "\t";
+	}
+
+	
 }
